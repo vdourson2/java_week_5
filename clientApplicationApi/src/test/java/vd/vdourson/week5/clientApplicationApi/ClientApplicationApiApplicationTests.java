1507b234d72009b69c605e1168e97ed57049a854
Virginie Dourson <virginiedourson@gmail.com>
2024-01-03 10:59:48 +0100

    Init challenge 2

---
+++ clientApplicationApi/src/test/java/vd/vdourson/week5/clientApplicationApi/ClientApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vd.vdourson.week5.clientApplicationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
