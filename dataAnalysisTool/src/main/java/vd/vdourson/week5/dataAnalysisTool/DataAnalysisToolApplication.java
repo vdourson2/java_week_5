a3f446ae33ce5664b35f9467cd717b1451fd24cc
Virginie Dourson <virginiedourson@gmail.com>
2024-01-03 15:21:41 +0100

    Init challenge 3

---
+++ dataAnalysisTool/src/main/java/vd/vdourson/week5/dataAnalysisTool/DataAnalysisToolApplication.java
@@ -0,0 +1,13 @@
+package vd.vdourson.week5.dataAnalysisTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalysisToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalysisToolApplication.class, args);
+	}
+
+}
---
+++ dataAnalysisTool/src/test/java/vd/vdourson/week5/dataAnalysisTool/DataAnalysisToolApplicationTests.java
@@ -0,0 +1,13 @@
+package vd.vdourson.week5.dataAnalysisTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalysisToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
